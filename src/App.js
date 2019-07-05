bd5b54b6aabec82861dbb7c010ee46eb46794455
noiach <gh@noiach.com>
2019-07-05 15:07:53 +1000

    Part 3: RenSDK

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // @ts-nocheck
 
 import React from 'react';
+import RenSDK from "@renproject/ren";
 import Web3 from "web3";
 import './App.css';
 
@@ -15,6 +16,7 @@ class App extends React.Component {
     this.state = {
       balance: 0,
       message: "",
+      sdk: new RenSDK("testnet"),
     }
   }
 
@@ -78,11 +80,85 @@ class App extends React.Component {
   }
 
   deposit = async () => {
-    // TODO
+    const { web3, sdk } = this.state;
+    const amount = 0.001; // BTC
+
+    const shiftIn = sdk.shiftIn({
+      // Send BTC from the Bitcoin blockchain to the Ethereum blockchain.
+      sendToken: RenSDK.Tokens.BTC.Btc2Eth,
+
+      // Amount of BTC we are sending (in Satoshis)
+      sendAmount: amount * (10 ** 8), // Convert to Satoshis
+
+      // The contract we want to interact with
+      sendTo: contractAddress,
+
+      // The name of the function we want to call
+      contractFn: "deposit",
+
+      // Arguments expected for calling `deposit`
+      contractParams: [
+        {
+          name: "_msg",
+          type: "bytes",
+          value: web3.utils.fromAscii(`Depositing ${amount} BTC`),
+        }
+      ],
+    });
+
+    // Show the gateway address to the user so that they can transfer their BTC to it.
+    const gatewayAddress = shiftIn.addr();
+    this.log(`Deposit ${amount} BTC to ${gatewayAddress}`);
+
+    // Wait for the Darknodes to detect the BTC transfer.
+    const confirmations = 0;
+    const deposit = await shiftIn.waitForDeposit(confirmations);
+
+    // Retrieve signature from RenVM.
+    this.log("Submitting to RenVM...");
+    const signature = await deposit.submitToRenVM();
+
+    // Submit the signature to Ethereum and receive zBTC.
+    this.log("Submitting to smart contract...");
+    await signature.submitToEthereum(web3.currentProvider);
+    this.log(`Deposited ${amount} BTC.`);
   }
 
   withdraw = async () => {
-    // TODO
+    const { web3, sdk, balance } = this.state;
+
+    const amount = balance;
+    const recipient = prompt("Enter BTC recipient:");
+    const from = (await web3.eth.getAccounts())[0];
+    const contract = new web3.eth.Contract(ABI, contractAddress);
+
+    this.log("Calling `withdraw` on smart contract...");
+    const txHash = await new Promise((resolve, reject) => {
+      contract.methods.withdraw(
+        web3.utils.fromAscii(`Depositing ${amount} BTC`), // _msg
+        RenSDK.Tokens.BTC.addressToHex(recipient), //_to
+        amount * (10 ** 8), // _amount in Satoshis
+      ).send({ from })
+        .on("transactionHash", resolve)
+        .catch(reject);
+    });
+
+    this.log(`Retrieving burn event from contract...`);
+    const shiftOut = await sdk.shiftOut({
+      // Send BTC from the Ethereum blockchain to the Bitcoin blockchain.
+      // This is the reverse of shitIn.
+      sendToken: RenSDK.Tokens.BTC.Eth2Btc,
+
+      // The web3 provider to talk to Ethereum
+      web3Provider: web3.currentProvider,
+
+      // The transaction hash of our contract call
+      txHash,
+    });
+
+    this.log(`Submitting to Darknodes...`);
+    await shiftOut.submitToRenVM();
+    this.log(`Withdrew ${amount} BTC to ${recipient}.`);
   }
 }
 
