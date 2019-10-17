abe0da8d6d6fd1475059fa4f18bbce5deb6ce5ea
noiach <gh@noiach.com>
2019-10-17 11:56:44 +1100

    Part 2: Added log for deposit call

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <p>Balance: {balance} BTC</p>
-        <p><button onClick={this.deposit}>Deposit 0.001 BTC</button></p>
+        <p><button onClick={() => this.deposit().catch(this.logError)}>Deposit 0.001 BTC</button></p>
         <p><button onClick={() => this.withdraw().catch(this.logError)}>Withdraw {balance} BTC</button></p>
         <p>{message}</p>
         {error ? <p style={{ color: "red" }}>{error}</p> : null}
@@ -93,6 +93,7 @@ class App extends React.Component {
   }
 
   deposit = async () => {
+    this.logError("");
     // TODO
   }
 
